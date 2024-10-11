cabc49258bd2b19fff5a0b4cbd5f7e7533b6ec8c
KAR16 <kherrera16@gmail.com>
2024-10-11 11:18:10 -0600

    Lista la parte 2 del proyecto integrando la conexión a MySQL

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using ApiNet8.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string? cadena = builder.Configuration.GetConnectionString("MySQLConnection") ?? "DefaultValue";
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<Conexiones>(opt =>
     //opt.UseInMemoryDatabase("TodoList")); --> Para manejar los datos en memoria
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); //-->Para manejar los datos con la cadena de conexión a Base de Datos
-
+    //opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); //-->Para manejar los datos con la cadena de conexión a Base de Datos SQL SERVER
+    opt.UseMySQL(cadena)); //--> Para manejar los datos con la cadena de conexión a MySQL
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
